e79397c612a1a36b6637604ebbf417e059351c97
Вячеслав Находнов <slava.nah91@gmail.com>
2023-02-07 08:50:24 +0300

    Была создана модель данных по недоделанной бд, после чего она была удалена

---
+++ ClassHelper/EFClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClothingStore.DB;
+
+namespace ClothingStore.ClassHelper
+{
+    public class EFClass
+    {
+        public static Entities Context {get;} = new Entities();
+    }
+}
--- Windows/LoginWindow.xaml.cs
+++ Windows/LoginWindow.xaml.cs
@@ -1,22 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using static ClothingStore.ClassHelper.EFClass;
 
 namespace ClothingStore.Windows
 {
-    /// <summary>
-    /// Логика взаимодействия для LoginWindow.xaml
-    /// </summary>
     public partial class LoginWindow : Window
     {
         public LoginWindow()
