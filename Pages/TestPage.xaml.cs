04ffa7982fdf0c409bfed0d0b0f40308fba33c3a
Вячеслав Находнов <slava.nah91@gmail.com>
2023-02-09 20:42:49 +0300

    Добавлено страничка с тестовым списком товаров

---
+++ Pages/TestPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using ClothingStore.ClassHelper;
+using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace ClothingStore.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для TestPage.xaml
+    /// </summary>
+    public partial class TestPage : Page
+    {
+        public TestPage()
+        {
+            InitializeComponent();
+
+            GetListProduct();
+        }
+
+        private void GetListProduct()
+        {
+            //List<Product> products = new List<Product>();
+            //products = EFClass.Content.Product.ToList();
+
+            //lvProduct.ItemsSource = EFClass.Context.Product.ToList();
+
+
+        }
+    }
+}
